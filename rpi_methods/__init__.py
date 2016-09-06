f0c4ce3f75aa184df3040348df4979c1b2a7328b
Giulia Bianchi <giuliabianchi@Giulias-MacBook-Pro.local>
2016-09-06 07:54:34 +0200

    modifications to read wav files (read and convert to ogg with pydub, read again with librosa)

--- rpi_methods/__init__.py
+++ rpi_methods/__init__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import librosa
+import re
+import pydub
+import os
 
 __all__ = [
     'Reader'
@@ -19,13 +22,50 @@ class Reader:
 
     def read_audio_file(self):
         """
-        Read audio file using librosa package. librosa allows re-sampling to desired sample rate and conversion to mono.
+        Read audio file using librosa package. librosa allows resampling to desired sample rate and convertion to mono.
 
         :return:
-        * audio_data as numpy.ndarray. A two-dimensional NumPy array is returned, where the channels are stored
+        * audiodata as numpy.ndarray. A two-dimensional NumPy array is returned, where the channels are stored
         along the first dimension, i.e. as columns. If the sound file has only one channel, a one-dimensional array is
         returned.
-        * sr as int. The sample rate of the audio file [Hz]
+        * samplerate as int. The sample rate of the audio file
         """
 
-        return librosa.load(self.file_name, sr=44100, mono=True, duration=5.0)
+        # Librosa doesn't work for wav files on python 2: https://github.com/librosa/librosa/issues/390
+        # return librosa.load(self.file_name, sr=44100, mono=True, duration=5.0)
+
+        if self._is_mp3_or_ogg(self.file_name):
+            return librosa.load(self.file_name, sr=44100, mono=True, duration=5.0)
+        else:
+            wav_file_pydub = pydub.AudioSegment.from_file(self.file_name)
+
+            ogg_file_name = self._ogg_file_name(self.file_name)
+
+            with wav_file_pydub.export(ogg_file_name, format='ogg', codec='libvorbis', bitrate='192k') as wav_file:
+                wav_file.close()
+
+            return librosa.load(ogg_file_name, sr=44100, mono=True, duration=5.0)
+
+    @staticmethod
+    def _is_mp3_or_ogg(string):
+        """
+        File name search to detect if the input file is mp3 or wav
+        :param string: file name as a string
+        :return: 1 (it's mp3 or ogg); 0 (it's wav or something alse)
+        """
+
+        return re.search('.+\.mp3|.+\.ogg', string)
+
+    @staticmethod
+    def _ogg_file_name(string):
+        """
+        Split the path to file as 'path/to/file' and 'file_name.wav' and transforms 'file_name.wav' to file_name.ogg'
+        :param string: file name as a string
+        :return: path to new file_name
+        """
+
+        head, tail = os.path.split(string)
+
+        new_tail = '.'.join([re.split('\.', tail)[0], 'ogg'])
+
+        return os.path.join(head, new_tail)
